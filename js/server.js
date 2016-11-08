f280456d8d82f87d276a25754f79b1d2fe9b5479
gingernutter <yangmei.chen2011@gmail.com>
2016-11-08 22:18:13 +1100

    port

--- js/server.js
+++ js/server.js
@@ -28,8 +28,7 @@ app.post("/upload", upload.single('myfile'), function (req, res, next) {
     });
 });
 
-app.set( 'port', ( process.env.PORT || 3000 ));
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Server is listening on port 3000. Ready to accept requests!');
 });
